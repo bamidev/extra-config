912c657f7ca674124efe7d92f56f566bcea94d5a
Bamidev <bamidev@pm.me>
2026-06-27 17:38:00 +0200

    Expand the hyprland config

--- hyprland/main.lua
+++ hyprland/main.lua
@@ -3,6 +3,45 @@ debug.disable_logs = false
 require('lua.binds')
 require('lua.plugin.hy3')
 
+
+
+hl.config({
+	general = {
+		border_size = 2,
+		gaps_in = 2,
+		gaps_out = 7,
+		layout = 'hy3',
+		resize_on_border = true,
+	},
+
+	input = {
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
+
+	misc = {
+		disable_hyprland_logo = true,
+	},
+
+	plugin = {
+		hy3 = {
+			autotile = {
+				enable = true
+			},
+
+			tab_first_window = true,
+
+			tabs = {
+				blur = false,
+				border_width = 1,
+				radius = 4,
+			},
+		}
+	},
+})
+
+
 --hl.on('hyprland.start', function()
 	hl.exec_cmd(
 		"swayidle -w " ..
--- hyprland/plugin/hy3.lua
+++ hyprland/plugin/hy3.lua
@@ -1,17 +1,2 @@
 require('lua.plugin.hy3.binds')
 
-hl.config({
-	general = {
-		layout = 'hy3',
-	},
-
-	plugin = {
-		hy3 = {
-			autotile = {
-				enable = true
-			},
-
-			tab_first_window = true,
-		}
-	}
-})
