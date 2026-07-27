4d72ecd99122c3aa548c10d647fe1afb5560410e
Bamidev <bamidev@pm.me>
2026-07-27 11:52:10 +0200

    Fix blink.cmp errors for new installs

--- neovim/lua/lsp/capabilities.lua
+++ neovim/lua/lsp/capabilities.lua
@@ -1,1 +1,8 @@
-return require('blink.cmp').get_lsp_capabilities()
+-- blink.cmp may not be available yet because at this point it may not have been installed yet.
+local success, blink = pcall(require, 'blink.cmp')
+if success then
+	return blink.get_lsp_capabilities()
+else
+	print('Unable to load blink.cmp (yet).')
+	return {}
+end
